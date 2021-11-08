c67c19f8b63db56f2f649123a9c9f072c799a3d1
Simon Farner <farnesim@students.zhaw.ch>
2021-11-08 15:40:24 +0100

    Import Spring Boot MVC Application

---
+++ src/test/java/com/example/mse/itsec/demonstrationservice/DemonstrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mse.itsec.demonstrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
