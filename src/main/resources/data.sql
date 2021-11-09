59dda0cfae05be08ccbb17e98e281a038bd74568
Simon Farner <farnesim@students.zhaw.ch>
2021-11-09 09:26:33 +0100

    DB is now correctly initialized

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,9 @@
-DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
-    id LONG AUTO_INCREMENT PRIMARY KEY,
+    id LONG AUTO_INCREMENT  PRIMARY KEY,
     username VARCHAR(250) NOT NULL,
-    password VARCHAR(256)
+    password VARCHAR(250) DEFAULT NULL
 );
 
-INSERT INTO users (username, password) Values
-    ('user', 'password')
+INSERT INTO users (username, password) VALUES
+                                              ('user', 'password'),
+                                              ('hans', 'wurst');
