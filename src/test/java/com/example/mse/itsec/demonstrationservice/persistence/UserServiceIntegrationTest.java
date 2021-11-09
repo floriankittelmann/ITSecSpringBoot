51b2c9f701bfabe0959bcb15624204f68d8e9e0d
Simon Farner <farnesim@students.zhaw.ch>
2021-11-09 09:13:56 +0100

    Unit Tests now work - yeay!

--- src/test/java/com/example/mse/itsec/demonstrationservice/persistence/UserServiceIntegrationTest.java
+++ src/test/java/com/example/mse/itsec/demonstrationservice/persistence/UserServiceIntegrationTest.java
@@ -4,11 +4,15 @@ import com.example.mse.itsec.demonstrationservice.persistence.dto.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.transaction.Transactional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 class UserServiceIntegrationTest {
