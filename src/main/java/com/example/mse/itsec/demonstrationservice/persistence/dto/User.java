37025aa450d7c34bfb3f6d0ab52700c9a1c415dc
Simon Farner <farnesim@students.zhaw.ch>
2021-11-15 21:04:04 +0100

    Added validation and email address to User

--- src/main/java/com/example/mse/itsec/demonstrationservice/persistence/dto/User.java
+++ src/main/java/com/example/mse/itsec/demonstrationservice/persistence/dto/User.java
@@ -3,6 +3,10 @@ package com.example.mse.itsec.demonstrationservice.persistence.dto;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -13,14 +17,21 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    @NotBlank(message = "username must not be blank")
+    @Size(min = 3, max = 100, message = "Username must be between 3 and 20 characters long")
     @Column(name = "username")
     private String username;
 
     @Column(name = "password")
     private String password;
+
+    @NotBlank(message = "e-mail address must not be blank")
+    @Email(message = "e-mail address must be a in a valid format")
+    @Column(name = "email")
+    private String email;
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE USER (
     id LONG AUTO_INCREMENT  PRIMARY KEY,
     username VARCHAR(250) NOT NULL,
-    password VARCHAR(250) DEFAULT NULL
+    password VARCHAR(250) DEFAULT NULL,
+    email VARCHAR(250) NOT NULL
 );
 
-INSERT INTO USER (username, password) VALUES
-                                              ('user', 'password'),
-                                              ('hans', 'wurst');
+INSERT INTO USER (username, password, email) VALUES
+                                              ('user', 'password','user@example.com'),
+                                              ('hans', 'wurst', 'hans@wurst.info');
