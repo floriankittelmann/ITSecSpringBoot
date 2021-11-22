f30f2723a2b89f829c386f8690a5e31a3334c535
Simon Farner <farnesim@students.zhaw.ch>
2021-11-22 20:54:33 +0100

    Added login page and controller

---
+++ src/main/java/com/example/mse/itsec/demonstrationservice/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.mse.itsec.demonstrationservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage(Model model, @RequestParam(value = "error", required = false) String error,
+                            @RequestParam(value = "logout", required = false) String logout) {
+        if (error != null) {
+            model.addAttribute("error", "Invalid Credentials");
+        }
+        if (logout != null) {
+            model.addAttribute("msg", "You have been successfully logged out");
+        }
+        return "login";
+    }
+    @PostMapping("/logout")
+    public String logoutPage(Model model, HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/login?logout";
+    }
+}
